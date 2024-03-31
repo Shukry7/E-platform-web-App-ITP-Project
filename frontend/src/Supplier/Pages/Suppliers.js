47f2f1fc276db01957f26e0f89f95f168b76f342
MohamedMufeez <129639317+MohamedMufeez@users.noreply.github.com>
2024-03-31 14:42:55 +0530

    supplier

--- frontend/src/Supplier/Pages/Suppliers.js
+++ frontend/src/Supplier/Pages/Suppliers.js
@@ -1,42 +1,50 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import SupplierTable from "./Components/SupplierTable";
-import Card from "../../Shared/Components/UiElements/Card"
+import Card from "../../Shared/Components/UiElements/Card";
 import Navbar from "../../Shared/Components/UiElements/Navbar";
 import { Link } from "react-router-dom";
-import {MdOutlineAddBox} from 'react-icons/md'
+import { MdOutlineAddBox } from "react-icons/md";
 
 const Suppliers = () => {
+  const [suppliers, setsupplier] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const [suppliers , setsupplier] = useState([]);
-  const [loading , setLoading] = useState(false)
-  
-  useEffect(() =>{
-    setLoading(true)
+  useEffect(() => {
+    setLoading(true);
     axios
-    .get("http://localhost:5000/supplier")
-    .then(res => {
-      setsupplier(res.data)
-      setLoading(false)
-    })
-    .catch(err => {
-      console.error(err)
-      setLoading(false)});
-
-  },[])
+      .get("http://localhost:5000/supplier")
+      .then((res) => {
+        setsupplier(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
+  }, []);
   return (
     <>
-    <Navbar/>
-   
-    <Card style={{width: "50%"}}>
-    <div className='flex justify-between items-center'>
-      <h1 className='text-3xl my-8'>Supplier List</h1>
-      <Link to='/Supplier/create'>
-        <MdOutlineAddBox className='text-sky-800 text-4xl'/>
-      </Link>
-    </div>
-      <SupplierTable Suppliers={suppliers} loading={loading} setloading={setLoading}/>
-      </Card>
+      <div className="flex flex-col lg:flex-row">
+        <div className="lg:w-1/4">
+          <Navbar />
+        </div>
+        <div className="lg:w-3/4 p-4">
+          <Card style={{ width: "90%" }}>
+            <div className="flex justify-between items-center">
+              <h1 className="text-3xl my-8">Supplier List</h1>
+              <Link to="/Supplier/create">
+                <MdOutlineAddBox className="text-sky-800 text-4xl" />
+              </Link>
+            </div>
+            <SupplierTable
+              Suppliers={suppliers}
+              loading={loading}
+              setloading={setLoading}
+            />
+          </Card>
+        </div>
+      </div>
     </>
   );
 };
