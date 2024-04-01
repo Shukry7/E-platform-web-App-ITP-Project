a23f820240f700266b4bf8045483237c868eaba8
FA Sukry <129639308+DedSec-24@users.noreply.github.com>
2024-04-01 20:28:38 +0530

    CRud ka hukum

--- frontend/src/Product/Pages/Components/ProductTable.jsx
+++ frontend/src/Product/Pages/Components/ProductTable.jsx
@@ -7,22 +7,9 @@ import Loader from "../../../Shared/Components/UiElements/Loader";
 import Table from "../../../Shared/Components/UiElements/Table";
 import TableRow from "../../../Shared/Components/UiElements/TableRow";
 import Popup from "./Popup";
+import ThreeDotDropdown from "../../../Shared/Components/UiElements/ThreeDotDropdown";
 
 const ProductTable = (props) => {
-  const deleteHandle = (id) => {
-    props.setloading(true);
-    axios
-      .delete(`http://localhost:5000/product/${id}`)
-      .then((res) => {
-        props.setloading(false);
-        Navigate("/Product");
-      })
-      .catch((err) => {
-        console.error(err);
-        props.setloading(false);
-      });
-  };
-
   const Headings = [
     "#",
     "Product ID",
@@ -37,38 +24,40 @@ const ProductTable = (props) => {
   return (
     <>
       <Table Headings={Headings}>
-          {props.loading ? (
-            <center>
-              <Loader />
-            </center>
-          ) : (
-            props.Product.map((item, index) => {
-              return (
-                <TableRow>
-                  <td class="px-6 py-4">{index + 1}</td>
-                  <td class="px-6 py-4">{item.ID}</td>
-                  <th
-                    scope="row"
-                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    {item.name}
-                  </th>
-                  <td class="px-6 py-4">{item.category}</td>
-                  <td class="px-6 py-4">Rs.{item.price}</td>
-                  <td class="px-6 py-4">{item.Stock}</td>
-                  <td class="px-6 py-4">{item.Alert_quantity}</td>
-                  <td class="px-6 py-4">
-                    <a
-                      href="#"
-                      class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                    >
-                      Edit
-                    </a>
-                  </td>
-                </TableRow>
-              );
-            })
-          )}
+        {props.loading ? (
+          <center>
+            <Loader />
+          </center>
+        ) : (
+          props.Product.map((item, index) => {
+            return (
+              <TableRow>
+                <td class="px-6 py-4">{index + 1}</td>
+                <td class="px-6 py-4">{item.ID}</td>
+                <th
+                  scope="row"
+                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {item.name}
+                </th>
+                <td class="px-6 py-4">{item.category}</td>
+                <td class="px-6 py-4">
+                  Rs.{item.price}
+                  <Popup id={item._id} />
+                </td>
+                <td class="px-6 py-4">{item.Stock}</td>
+                <td class="px-6 py-4">{item.Alert_quantity}</td>
+                <td class="px-6 py-4">
+                  <ThreeDotDropdown
+                    link1=""
+                    link2={`/Product/update/` + item._id}
+                    id={item._id}
+                  />
+                </td>
+              </TableRow>
+            );
+          })
+        )}
       </Table>
     </>
   );
