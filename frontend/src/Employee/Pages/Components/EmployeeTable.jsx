66cb992181e303b27e8e094cefeca51441a2ee61
Faizal Amaan <faizalamaan2@gmail.com>
2024-04-01 23:22:36 +0530

    done

--- frontend/src/Employee/Pages/Components/EmployeeTable.jsx
+++ frontend/src/Employee/Pages/Components/EmployeeTable.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye, faPenToSquare } from "@fortawesome/free-regular-svg-icons";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
-
 import "./employeeTable.css";
-import Button from "../../../Shared/Components/FormElements/Button";
 import axios from "axios";
 import { Navigate } from "react-router-dom";
 import Loader from "../../../Shared/Components/UiElements/Loader";
+import Table from "../../../Shared/Components/UiElements/Table";
+import TableRow from "../../../Shared/Components/UiElements/TableRow";
+import { Link } from "react-router-dom";
+import { MdDeleteForever } from "react-icons/md";
 
 const EmployeeTable = (props) => {
   const deleteHandle = (id) => {
@@ -24,73 +23,56 @@ const EmployeeTable = (props) => {
       });
   };
 
+  const Headings = [
+    "#",
+    "Employee ID",
+    "Employee name",
+    "Address",
+    "Telephone",
+    "Type",
+    "Hourly Wage",
+    "Action",
+  ];
+
   return (
     <>
-      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" class="px-6 py-3">
-                #Employee ID
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Employee name
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Address
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Phone number
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Type
-              </th>
-              <th scope="col" class="px-6 py-3">
-                HourlyWage
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Action
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {props.loading ? (
-              <center>
-                <Loader />
-              </center>
-            ) : (
-              props.Employee.map((item, index) => {
-                return (
-                  <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                    <td class="px-6 py-4">{index + 1}</td>
-                    <th
-                      scope="row"
-                      class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      {item.name}
-                    </th>
-                    <td class="px-6 py-4">{item.address}</td>
-                    <td class="px-6 py-4">Rs.{item.telephone}</td>
-                    <td class="px-6 py-4">{item.type}</td>
-                    <td class="px-6 py-4">{item.hourlywage}</td>
-                    <td class="px-6 py-4">
-                      <a
-                        href="#"
-                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                      >
-                        Edit
-                      </a>
-                    </td>
-                    
-                  </tr>
-                );
-              })
-            )}
-          </tbody>
-        </table>
-      </div>
+      <Table Headings={Headings}>
+          {props.loading ? (
+            <center>
+              <Loader />
+            </center>
+          ) : (
+            props.Employee.map((item, index) => {
+              return (
+                <TableRow>
+                  <td class="px-6 py-4">{index + 1}</td>
+                  <td class="px-6 py-4">{item.ID}</td>
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {item.name}
+                  </th>
+                  <td class="px-6 py-4">{item.address}</td>
+                  <td class="px-6 py-4">{item.telephone}</td>
+                  <td class="px-6 py-4">{item.type}</td>
+                  <td class="px-6 py-4">{item.hourlywage}</td>
+                  <td class="px-6 py-4 flex justify-center gap-2">
+                    <Link to={'/Employee/delete/'+item._id}>
+                      <MdDeleteForever />
+                    </Link>
+                    <Link to={'/Employee/delete/'+item._id}>
+                      <MdDeleteForever />
+                    </Link>
+                  </td>
+                </TableRow>
+              );
+            })
+          )}
+      </Table>
     </>
   );
 };
 
 export default EmployeeTable;
+
