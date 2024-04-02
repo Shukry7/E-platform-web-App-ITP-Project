41f27820b2a82a4bc3110c763a3fb45fc0582bb2
MohamedMufeez <129639317+MohamedMufeez@users.noreply.github.com>
2024-04-02 22:47:28 +0530

    app.js changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Updateproduct from "./Product/Pages/Updateproduct";
 import Suppliers from "./Supplier/Pages/Suppliers";
 import CreateSupplier from "./Supplier/Pages/CreateSupplier";
 import Updatesupplier from "./Supplier/Pages/Updatesupplier";
+import ViewSupplier from "./Supplier/Pages/ViewSupplier";
 import Delivery from "./Delivery/Pages/Delivery";
 import CreateDelivery from "./Delivery/Pages/CreateDelivery";
 import UpdateDelivery from "./Delivery/Pages/UpdateDelivery";
@@ -28,6 +29,7 @@ const App = () => {
         <Route path="/Supplier" exact element={<Suppliers/>}/>
         <Route path="/Supplier/create" exact element={<CreateSupplier/>}/>
         <Route path="/Supplier/update/:id" exact element={<Updatesupplier/>}/>
+        <Route path="/Supplier/view/:id" exact element={<ViewSupplier/>}/>
         <Route path="/Delivery" exact element={<Delivery/>}/>
         <Route path="/Delivery/create" exact element={<CreateDelivery/>}/>
         <Route path="/Delivery/update/:id" exact element={<UpdateDelivery/>}/>
---
+++ frontend/src/Supplier/Pages/ViewSupplier.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ViewSupplier = () => {
+  return (
+    <div>ViewSupplier</div>
+  )
+}
+
+export default ViewSupplier;
