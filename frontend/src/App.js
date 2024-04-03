6d99551bdc426119268c0ea84d6f4c669d6896b9
FA Sukry <129639308+DedSec-24@users.noreply.github.com>
2024-04-03 22:51:15 +0530

    overwrite

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import {
 } from "react-router-dom";
 import AddProduct from "./Product/Pages/Addproduct";
 import Products from "./Product/Pages/Products";
-import Updateproduct from "./Product/Pages/Updateproduct";
 import Suppliers from "./Supplier/Pages/Suppliers";
 import DeleteSupplier from "./Supplier/Pages/DeleteSupplier";
 import CreateSupplier from "./Supplier/Pages/CreateSupplier";
@@ -15,6 +14,8 @@ import CreateDelivery from "./Delivery/Pages/CreateDelivery";
 import AddEmployee from "./Employee/Pages/AddEmployee";
 import Employee from "./Employee/Pages/Employee";
 import Updateemployee from "./Employee/Pages/Updateemployee";
+import UpdateProduct from "./Product/Pages/UpdateProduct";
+import ViewProduct from "./Product/Pages/ViewProduct";
 
 
 const App = () => {
@@ -23,8 +24,9 @@ const App = () => {
     <Router>
       <Routes >
         <Route path="/Product" exact element={<Products/>}/>
-        <Route path="/Product/update/:id" exact element={<Updateproduct/>}/> 
+        <Route path="/Product/update/:id" exact element={<UpdateProduct/>}/> 
         <Route path="/Product/new" exact element={<AddProduct/>}/>
+        <Route path="/Product/Details/:id" exact element={<ViewProduct/>}/>
         <Route path="/Supplier" exact element={<Suppliers/>}/>
         <Route path="/Supplier/delete/:id" exact element={<DeleteSupplier/>}/>
         <Route path="/Supplier/create" exact element={<CreateSupplier/>}/>
