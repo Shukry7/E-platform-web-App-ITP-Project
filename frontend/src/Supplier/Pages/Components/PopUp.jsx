408efab6ac609faeecf52a755fa7c95fcfd6fa92
MohamedMufeez <129639317+MohamedMufeez@users.noreply.github.com>
2024-04-11 21:47:21 +0530

    Update PopUp.jsx

--- frontend/src/Supplier/Pages/Components/PopUp.jsx
+++ frontend/src/Supplier/Pages/Components/PopUp.jsx
@@ -37,11 +37,13 @@ function PopUp(props) {
         {
           unitPrice: {
             value: "",
-            isValid: true,
-          },
+            isValid: false,
+          }
         },
         false
       );
+
+      console.log(formState)
 
       const submitHandler = async (event) => {
         event.preventDefault();
@@ -149,6 +151,7 @@ function PopUp(props) {
                       <button
                         onClick={submitHandler}
                         type="submit"
+                        disabled={!formState.isValid}
                         class="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900"
                       >
                         Add Product
