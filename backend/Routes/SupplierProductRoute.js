b373d5bfd77dae63c6985c80dfbd39c9c748f041
FA Sukry <129639308+DedSec-24@users.noreply.github.com>
2024-04-11 20:09:05 +0530

    update

--- backend/Routes/SupplierProductRoute.js
+++ backend/Routes/SupplierProductRoute.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const SupplierProductControllers = require("../Controllers/SupplierProduct-controller");
 const Router = express.Router();
-const fileupload = require('../middleware/file-upload')
 
-Router.post("/",fileupload.single('image'), SupplierProductControllers.createSupplierProduct);
+Router.post("/", SupplierProductControllers.createSupplierProduct);
 Router.delete("/:id", SupplierProductControllers.DeleteSupplierProduct);
 Router.get("/:id", SupplierProductControllers.listProductBySupplierId);
 Router.get("/product/:id", SupplierProductControllers.listSupplierByProductId);
--- backend/Routes/SupplierRoute.js
+++ backend/Routes/SupplierRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const SupplierControllers = require("../Controllers/supplier-controllers");
 const Router = express.Router();
+const fileupload = require('../middleware/file-upload')
 
-Router.post("/", SupplierControllers.createSupplier);
+Router.post("/",fileupload.single('image'), SupplierControllers.createSupplier);
 Router.get("/", SupplierControllers.listSupplier);
 Router.delete("/:id", SupplierControllers.DeleteSupplier);
 Router.get("/:id", SupplierControllers.listSupplierById);
