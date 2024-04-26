15846d37f2c35debeb737e08fd010e5c345f5044
Faizal Amaan <faizalamaan2@gmail.com>
2024-04-27 00:15:21 +0530

    gi

--- frontend/src/Employee/Pages/Salaryform.js
+++ frontend/src/Employee/Pages/Salaryform.js
@@ -2,13 +2,16 @@ import React from "react";
 import Card from "../../Shared/Components/UiElements/Card";
 import Navbar from "../../Shared/Components/UiElements/Navbar";
 import SalaryCalculatorForm from "./Components/salary";
-
+import Header from "../../Shared/Components/UiElements/header";
 const Salaryform = () => {
   return (
     <>
      <Navbar />
+     <Header />
+     
+     <Card className="flex" style={{ width: "80%" }}>
 
-      <Card className="flex" style={{ width: "80%" ,height:"20%"}}>
+      
         <SalaryCalculatorForm />
       </Card>
     </>
