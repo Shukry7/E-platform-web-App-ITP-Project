04da858e067c6b0153ab22585e2480b7f119e469
MohamedMufeez <129639317+MohamedMufeez@users.noreply.github.com>
2024-04-26 16:22:05 +0530

    css

--- frontend/src/Supplier/Pages/Components/PopUpPurchase.jsx
+++ frontend/src/Supplier/Pages/Components/PopUpPurchase.jsx
@@ -83,7 +83,7 @@ function PopUpPurchase(props) {
                     <>
                         <div class="mt-5 relative overflow-x-auto max-h-[520px] max-w-[820px] min-h-96 shadow-md sm:rounded-lg ">
                         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                            <thead class="text-xs text-gray-50 uppercase bg-gray-600 dark:bg-gray-700 dark:text-gray-400">
+                            <thead class="text-xs text-gray-50 uppercase bg-red-400 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" class="px-6 py-3 font-semibold">Purchase ID</th>
                                 <th scope="col" class="px-6 py-3 font-semibold">Supplier</th>
