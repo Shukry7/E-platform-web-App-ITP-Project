c6616c4968f46378559be8553ade138e16da4fdc
MohamedMufeez <129639317+MohamedMufeez@users.noreply.github.com>
2024-05-17 01:21:58 +0530

    css

--- frontend/src/Supplier/Pages/Components/SupplierReportDetails.jsx
+++ frontend/src/Supplier/Pages/Components/SupplierReportDetails.jsx
@@ -74,7 +74,7 @@ const SupplierReportDetails = () => {
 
   return (
     <div className="container mx-auto my-8 mt-10">
-      <div className="flex items-center justify-between mt-40 mb-4">
+      <div className="flex items-center justify-between mb-4">
         <div className="mr-4">
           <label htmlFor="startDate" className="mr-2">
             Start Date:
