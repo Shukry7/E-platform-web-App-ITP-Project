ddbbeded73118f8230a77da37c9e9157a25fc240
lukemaxwell01 <158442051+lukemaxwell01@users.noreply.github.com>
2024-05-17 13:44:06 +0530

    Navbar Removal

--- frontend/src/Customer/RegisterCustomer.jsx
+++ frontend/src/Customer/RegisterCustomer.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Card from "../Shared/Components/UiElements/Card";
 import CustomerForm from "./Components/CustomerForm";
-import Navbar from "../Shared/Components/UiElements/Navbar";
 
 const RegisterCustomer = () => {
   return (
     <>
       <div>
-        <Navbar />
         <CustomerForm />
       </div>
     </>
