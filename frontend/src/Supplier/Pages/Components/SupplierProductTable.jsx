75a4cfe5fea44eca27a3c747ee21eb0e8594f1ba
MohamedMufeez <129639317+MohamedMufeez@users.noreply.github.com>
2024-05-17 21:40:38 +0530

    css

--- frontend/src/Supplier/Pages/Components/SupplierProductTable.jsx
+++ frontend/src/Supplier/Pages/Components/SupplierProductTable.jsx
@@ -10,6 +10,7 @@ import UpdatePrice from "./UpdatePrice";
 import { MdDeleteOutline } from "react-icons/md";
 import Toast from "../../../Shared/Components/UiElements/Toast/Toast";
 import { useReactToPrint } from 'react-to-print';
+import PrintDetails from "./PrintDetails";
 
 const SupplierProductTable = (props) => {
   const [cart, setCart] = useState([]);
@@ -99,6 +100,7 @@ const SupplierProductTable = (props) => {
 
   return (
     <>
+    <PrintDetails details={cart}  componentRef={componentRef} totalAmount={total}  paid = {amount}/>
       <div>
         <Table Headings={Headings}>
           {props.loading ? (
@@ -148,7 +150,7 @@ const SupplierProductTable = (props) => {
       </div>
 
       {isclick &&
-        <div ref={componentRef}>
+        <div>
           <div
             id="deleteModal"
             tabindex="-1"
@@ -179,6 +181,11 @@ const SupplierProductTable = (props) => {
                     </svg>
                     <span class="sr-only">Close modal</span>
                   </button>
+                  {loading && (
+                      <center>
+                        <Loader />
+                      </center>
+                    ) }
 
                   <div className="flex flex-row ...">
                     <div>
@@ -232,24 +239,40 @@ const SupplierProductTable = (props) => {
                     </div>
                   </div>
 
-                  <div class="flex  justify-center items-center space-x-4 pt-9">
-                    <button
-                      onClick={submitHandler}
-                      type="submit"
-                      
-                      class="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900"
-                    >
+                  <div class="flex justify-center items-center space-x-4 pt-9">
+                  <button
+                    onClick={submitHandler}
+                    type="submit"
+                    className="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900"
+                    disabled = {loading}
+                  >
                       Buy Products
-                    </button>
-                    <div className="flex justify-end">
+                  </button>
+                    <div className="flex justify-end flex-grow">
                       <button
                         onClick={handleprint}
-                        className="flex flex-row-reverse ..."                                          
+                        className="flex flex-row-reverse items-center space-x-2"
                       >
-                        <svg class="h-8 w-8 text-black-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />  <polyline points="14 2 14 8 20 8" />  <line x1="16" y1="13" x2="8" y2="13" />  <line x1="16" y1="17" x2="8" y2="17" />  <polyline points="10 9 9 9 8 9" /></svg>
+                        <span>Print</span>
+                        <svg
+                          class="h-8 w-8 text-black-500"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
+                          <polyline points="14 2 14 8 20 8" />
+                          <line x1="16" y1="13" x2="8" y2="13" />
+                          <line x1="16" y1="17" x2="8" y2="17" />
+                          <polyline points="10 9 9 9 8 9" />
+                        </svg>
                       </button>
                     </div>
                   </div>
+
                 </div>
               </div>
           </div>
